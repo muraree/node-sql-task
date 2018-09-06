305e9fc93fd975abea75cd75e4b7acc25500de68
Muraree <murareelalsahu@gmail.com>
2018-09-06 17:10:26 +0530

    modification in verify token file

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -4,10 +4,10 @@ const config = require('../config');
 function verifyToken(req, res, next) {
 
   const token = req.headers["x-access-token"];
-  if(!token) res.send({auth: false, message: "no token provided"});
+  if(!token) return res.send({auth: false, message: "no token provided"});
 
   jwt.verify(token, config.secret, function(err, decoded) {
-    if(err) res.send({auth: false, message: "token authentication failed"});
+    if(err) return res.send({auth: false, message: "token authentication failed"});
 
     req.userId = decoded.id;
     if(req.params && req.params.id && decoded.id !== parseInt(req.params.id)) return res.send({ auth: false, message: "user not found"});
